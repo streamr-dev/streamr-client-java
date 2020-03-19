5c36656eaa8fe45225b7f9ccdcad780840437b77
mthambipillai <melchior.thambipillai@streamr.com>
2020-03-19 14:42:23 +0100

    add get number of subscribers to revoke

--- src/main/java/com/streamr/client/utils/AddressValidityUtil.java
+++ src/main/java/com/streamr/client/utils/AddressValidityUtil.java
@@ -4,20 +4,27 @@ import org.cache2k.Cache;
 import org.cache2k.Cache2kBuilder;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /*
 This util contains methods to check if an Ethereum address is a valid publisher/subscriber (using REST endpoints).
+It also contains methods to get the number of subscribers to revoke for a stream.
  */
 public class AddressValidityUtil {
+    private static final int CACHE_EXPIRATION = 30; // in minutes
+    private final HashMap<String, HashSet<String>> localSubscribersSets = new HashMap<>();
     private final Cache<String, HashMap<String, Boolean>> subscribersPerStreamId = new Cache2kBuilder<String, HashMap<String, Boolean>>() {}
-            .expireAfterWrite(30, TimeUnit.MINUTES).build();
+            .expireAfterWrite(CACHE_EXPIRATION, TimeUnit.MINUTES).build();
     private final Function<String, List<String>> getSubscribersFunction;
     private final BiFunction<String, String, Boolean> isSubscriberFunction;
     private final Cache<String, HashMap<String, Boolean>> publishersPerStreamId = new Cache2kBuilder<String, HashMap<String, Boolean>>() {}
-            .expireAfterWrite(30, TimeUnit.MINUTES).build();
+            .expireAfterWrite(CACHE_EXPIRATION, TimeUnit.MINUTES).build();
     private final Function<String, List<String>> getPublishersFunction;
     private final BiFunction<String, String, Boolean> isPublisherFunction;
 
@@ -27,6 +34,18 @@ public class AddressValidityUtil {
         this.isSubscriberFunction = isSubscriberFunction;
         this.getPublishersFunction = getPublishersFunction;
         this.isPublisherFunction = isPublisherFunction;
+    }
+
+    public int nbSubscribersToRevoke(String streamId) {
+        HashSet<String> realSubscribersSet = new HashSet<>(getSubscribersFunction.apply(streamId));
+        int counter = 0;
+        for (String subscriberId: localSubscribersSets.getOrDefault(streamId, new HashSet<>())) {
+            if (!realSubscribersSet.contains(subscriberId)) {
+                counter++;
+            }
+        }
+        localSubscribersSets.put(streamId, realSubscribersSet);
+        return counter;
     }
 
     public boolean isValidSubscriber(String streamId, String subscriberId) {
@@ -78,7 +97,7 @@ public class AddressValidityUtil {
     private static Cache<String, HashMap<String, Boolean>> safeGetCache(Cache<String, HashMap<String, Boolean>> cache) {
         if (cache.isClosed()) {
             cache = new Cache2kBuilder<String, HashMap<String, Boolean>>() {}
-                    .expireAfterWrite(30, TimeUnit.MINUTES).build();
+                    .expireAfterWrite(CACHE_EXPIRATION, TimeUnit.MINUTES).build();
         }
         return cache;
     }
