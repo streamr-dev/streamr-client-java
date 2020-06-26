eecf84d12ea6bc657885f7c17f5dd30f40157756
Henri Pihkala <hpihkala@users.noreply.github.com>
2020-06-26 16:56:45 +0200

    delete StreamMessage v28 and v29 implementations

--- src/main/java/com/streamr/client/protocol/message_layer/StreamMessageAdapter.java
+++ src/main/java/com/streamr/client/protocol/message_layer/StreamMessageAdapter.java
@@ -6,19 +6,14 @@ import com.squareup.moshi.JsonReader;
 import com.squareup.moshi.JsonWriter;
 import com.streamr.client.exceptions.MalformedMessageException;
 import com.streamr.client.exceptions.UnsupportedMessageException;
-import okio.Buffer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.streamr.client.protocol.message_layer.StreamMessage.ContentType;
-import com.streamr.client.protocol.message_layer.StreamMessage.SignatureType;
 
 import java.io.IOException;
 
 public class StreamMessageAdapter extends JsonAdapter<StreamMessage> {
 
     private static final Logger log = LogManager.getLogger();
-    private static final StreamMessageV28Adapter v28Adapter = new StreamMessageV28Adapter();
-    private static final StreamMessageV29Adapter v29Adapter = new StreamMessageV29Adapter();
     private static final StreamMessageV30Adapter v30Adapter = new StreamMessageV30Adapter();
     private static final StreamMessageV31Adapter v31Adapter = new StreamMessageV31Adapter();
 
@@ -30,11 +25,7 @@ public class StreamMessageAdapter extends JsonAdapter<StreamMessage> {
             // Check version
             int version = reader.nextInt();
             StreamMessage msg;
-            if (version == StreamMessageV28.VERSION) {
-                msg = v28Adapter.fromJson(reader);
-            } else if (version == StreamMessageV29.VERSION) {
-                msg = v29Adapter.fromJson(reader);
-            } else if (version == StreamMessageV30.VERSION) {
+            if (version == StreamMessageV30.VERSION) {
                 msg = v30Adapter.fromJson(reader);
             } else if (version == StreamMessageV31.VERSION) {
                 msg = v31Adapter.fromJson(reader);
@@ -54,11 +45,7 @@ public class StreamMessageAdapter extends JsonAdapter<StreamMessage> {
         writer.beginArray();
         int version = value.getVersion();
         writer.value(version);
-        if (version == StreamMessageV28.VERSION) {
-            v28Adapter.toJson(writer, (StreamMessageV28) value);
-        } else if (version == StreamMessageV29.VERSION) {
-            v29Adapter.toJson(writer, (StreamMessageV29) value);
-        } else if (version == StreamMessageV30.VERSION) {
+        if (version == StreamMessageV30.VERSION) {
             v30Adapter.toJson(writer, (StreamMessageV30) value);
         } else if (version == StreamMessageV31.VERSION) {
             v31Adapter.toJson(writer, (StreamMessageV31) value);
