a474912f3dc4dc6764b443a47be7e9b2176d72b7
Henri Pihkala <hpihkala@users.noreply.github.com>
2020-07-06 17:58:05 +0200

    run shared key tests on both JS and Java clients in Travis

--- .travis_scripts/cross-client-tests.sh
+++ .travis_scripts/cross-client-tests.sh
@@ -39,7 +39,7 @@ echo "Run cross-client test scenarios"
 # TODO: change java-only-ci.conf to default-ci.conf once JS client is compatible
 java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -m test -s stream-cleartext-unsigned -c config/default-ci.conf && \
 java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -m test -s stream-cleartext-signed -c config/default-ci.conf && \
-java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -m test -s stream-encrypted-shared-signed -c config/java-only-ci.conf && \
-java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -m test -s stream-encrypted-shared-rotating-signed -c config/java-only-ci.conf && \
+java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -m test -s stream-encrypted-shared-signed -c config/default-ci.conf && \
+java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -m test -s stream-encrypted-shared-rotating-signed -c config/default-ci.conf && \
 java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -m test -s stream-encrypted-exchanged-rotating-signed -c config/java-only-ci.conf && \
 java -jar build/libs/client_testing-1.0-SNAPSHOT.jar -m test -s stream-encrypted-exchanged-rotating-revoking-signed -c config/java-only-ci.conf
