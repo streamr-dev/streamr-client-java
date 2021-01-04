91857dd9a37e7b75855d9267d4cde149bbcce541
Kare Nuorteva <kare.nuorteva@me.com>
2021-01-04 23:00:02 +0200

    Revert "Remove unused InvalidGroupKeyAnnounceException"
    This reverts commit 5e1258e6dada19fe54ddc30aed2f6638f355bb22.

---
+++ src/main/java/com/streamr/client/exceptions/InvalidGroupKeyAnnounceException.java
@@ -0,0 +1,7 @@
+package com.streamr.client.exceptions;
+
+public class InvalidGroupKeyAnnounceException extends Exception {
+    public InvalidGroupKeyAnnounceException(String msg) {
+        super(msg);
+    }
+}
