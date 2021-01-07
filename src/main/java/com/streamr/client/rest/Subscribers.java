ea7ce61b2fd0a36708620e7362a362d5bcecc34a
Kare Nuorteva <kare.nuorteva@me.com>
2021-01-07 15:50:18 +0200

    Fix Subscribers equals contract

--- src/main/java/com/streamr/client/rest/Subscribers.java
+++ src/main/java/com/streamr/client/rest/Subscribers.java
@@ -1,11 +1,29 @@
 package com.streamr.client.rest;
 
 import java.util.List;
+import java.util.Objects;
 
-public class Subscribers {
-    private List<String> addresses;
+public final class Subscribers {
+    private final List<String> addresses;
+
+    public Subscribers(final List<String> addresses) {
+        this.addresses = addresses;
+    }
 
     public List<String> getAddresses() {
         return addresses;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        final Subscribers that = (Subscribers) obj;
+        return Objects.equals(addresses, that.addresses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(addresses);
     }
 }
