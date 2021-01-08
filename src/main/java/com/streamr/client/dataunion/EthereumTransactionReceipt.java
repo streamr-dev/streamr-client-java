5943051ced6b56f435f3c98788918280da627db8
Kare Nuorteva <kare.nuorteva@me.com>
2021-01-08 18:10:43 +0200

    Improve Javadoc for EthereumTransactionReceipt

--- src/main/java/com/streamr/client/dataunion/EthereumTransactionReceipt.java
+++ src/main/java/com/streamr/client/dataunion/EthereumTransactionReceipt.java
@@ -2,16 +2,15 @@ package com.streamr.client.dataunion;
 
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
-
-/**
- * wrapper to avoid exposing web3j TransactionReceipt
- */
+/** Streamr Ethereum transaction receipt. */
 public class EthereumTransactionReceipt {
-    TransactionReceipt tr;
-    EthereumTransactionReceipt(TransactionReceipt tr){
-        this.tr = tr;
-    }
-    public String getTransactionHash(){
-        return tr.getTransactionHash();
-    }
+  final TransactionReceipt tr;
+
+  EthereumTransactionReceipt(final TransactionReceipt tr) {
+    this.tr = tr;
+  }
+
+  public String getTransactionHash() {
+    return tr.getTransactionHash();
+  }
 }
