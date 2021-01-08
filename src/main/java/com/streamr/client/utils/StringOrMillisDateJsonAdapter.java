61343890c0ebd1af35e8974fad1ad2a595ae0fd4
Kare Nuorteva <kare.nuorteva@streamr.com>
2021-01-08 19:43:21 +0200

    NET-151: Add java.time.Instant JSON adapter (#99)
    * NET-151: Add JSON adapter for java.time.Instant
    
    * Moshi adapters don't need to be synchronized
    
    * Release v2.0.3-alpha.2

--- src/main/java/com/streamr/client/utils/StringOrMillisDateJsonAdapter.java
+++ src/main/java/com/streamr/client/utils/StringOrMillisDateJsonAdapter.java
@@ -3,25 +3,24 @@ package com.streamr.client.utils;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.JsonReader;
 import com.squareup.moshi.JsonWriter;
-
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Date;
 
-/**
- * Formats dates as milliseconds since epoch, similarly to Date#getTime().
- */
+/** Formats dates as milliseconds since epoch, similarly to {@code Date#getTime()}. */
 public final class StringOrMillisDateJsonAdapter extends JsonAdapter<Date> {
-    @Override public synchronized Date fromJson(JsonReader reader) throws IOException {
-        // Peek at the value to determine its type
-        if (reader.peek().equals(JsonReader.Token.NUMBER)) {
-            return new Date(reader.nextLong());
-        } else {
-            return Date.from(Instant.parse(reader.nextString()));
-        }
+  @Override
+  public Date fromJson(JsonReader reader) throws IOException {
+    // Peek at the value to determine its type
+    if (reader.peek().equals(JsonReader.Token.NUMBER)) {
+      return new Date(reader.nextLong());
+    } else {
+      return Date.from(Instant.parse(reader.nextString()));
     }
+  }
 
-    @Override public synchronized void toJson(JsonWriter writer, Date value) throws IOException {
-        writer.value(String.valueOf(value.getTime()));
-    }
+  @Override
+  public void toJson(JsonWriter writer, Date value) throws IOException {
+    writer.value(String.valueOf(value.getTime()));
+  }
 }
