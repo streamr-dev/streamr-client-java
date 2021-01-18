141fdb61ab95f599366137783b4ea924be2392bf
Kare Nuorteva <kare.nuorteva@streamr.com>
2021-01-18 16:40:33 +0200

    NET-161: Extract common test helpers (#105)
    * Move generatePrivateKey() to testing.TestingKeys
    
    * Move URL constants to TestingMeta
    
    * Extract TestingStreams.generateName()
    
    * Make createOptionsWithPrivateKey() private
    
    * Extract TestingClient
    
    * Remove StreamrIntegrationSpecification
    
    * Remove temporary TestingClient
    
    * Make parameters final

--- src/integrationTest/groovy/com/streamr/client/testing/TestingStreamrClient.java
+++ src/integrationTest/groovy/com/streamr/client/testing/TestingStreamrClient.java
@@ -1,7 +1,12 @@
-package com.streamr.client;
+package com.streamr.client.testing;
 
+import com.streamr.client.MessageHandler;
+import com.streamr.client.StreamrClient;
+import com.streamr.client.authentication.EthereumAuthenticationMethod;
 import com.streamr.client.exceptions.ResourceNotFoundException;
+import com.streamr.client.options.EncryptionOptions;
 import com.streamr.client.options.ResendOption;
+import com.streamr.client.options.SigningOptions;
 import com.streamr.client.options.StreamrClientOptions;
 import com.streamr.client.protocol.control_layer.ControlMessage;
 import com.streamr.client.protocol.message_layer.StreamMessage;
@@ -19,8 +24,31 @@ public class TestingStreamrClient extends StreamrClient {
   List<StreamMessage> receivedStreamMessages = new ArrayList<>();
   Map<String, Stream> mockStreams = new LinkedHashMap<>();
 
-  public TestingStreamrClient(StreamrClientOptions options) {
+  public TestingStreamrClient(final StreamrClientOptions options) {
     super(options);
+  }
+
+  public static StreamrClient createUnauthenticatedClient() {
+    return new StreamrClient(
+        new StreamrClientOptions(
+            null,
+            SigningOptions.getDefault(),
+            EncryptionOptions.getDefault(),
+            TestingMeta.WEBSOCKET_URL,
+            TestingMeta.REST_URL));
+  }
+
+  public static StreamrClient createClientWithPrivateKey(final String privateKey) {
+    return new StreamrClient(createOptionsWithPrivateKey(privateKey));
+  }
+
+  private static StreamrClientOptions createOptionsWithPrivateKey(final String privateKey) {
+    return new StreamrClientOptions(
+        new EthereumAuthenticationMethod(privateKey),
+        SigningOptions.getDefault(),
+        EncryptionOptions.getDefault(),
+        TestingMeta.WEBSOCKET_URL,
+        TestingMeta.REST_URL);
   }
 
   @Override
