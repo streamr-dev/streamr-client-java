41b3ac031fdb4f1760f1a938db4d6e99e11e8675
Kare Nuorteva <kare.nuorteva@me.com>
2021-01-21 16:20:01 +0200

    Apply source format

--- src/main/java/com/streamr/client/subs/RealTimeSubscription.java
+++ src/main/java/com/streamr/client/subs/RealTimeSubscription.java
@@ -2,82 +2,104 @@ package com.streamr.client.subs;
 
 import com.streamr.client.MessageHandler;
 import com.streamr.client.exceptions.GapDetectedException;
-import com.streamr.client.protocol.common.UnsupportedMessageException;
 import com.streamr.client.options.ResendOption;
+import com.streamr.client.protocol.common.UnsupportedMessageException;
 import com.streamr.client.protocol.message_layer.StreamMessage;
 import com.streamr.client.utils.*;
+import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-
 public class RealTimeSubscription extends BasicSubscription {
 
-    private static final Logger log = LoggerFactory.getLogger(RealTimeSubscription.class);
+  private static final Logger log = LoggerFactory.getLogger(RealTimeSubscription.class);
 
-    private boolean resending = false;
+  private boolean resending = false;
 
-    public RealTimeSubscription(String streamId,
-                                int partition,
-                                MessageHandler handler,
-                                GroupKeyStore keyStore,
-                                KeyExchangeUtil keyExchangeUtil,
-                                GroupKeyRequestFunction groupKeyRequestFunction,
-                                long propagationTimeout,
-                                long resendTimeout,
-                                boolean skipGapsOnFullQueue) {
-        super(streamId, partition, handler, keyStore, keyExchangeUtil, groupKeyRequestFunction, propagationTimeout, resendTimeout,
-                skipGapsOnFullQueue);
-    }
+  public RealTimeSubscription(
+      String streamId,
+      int partition,
+      MessageHandler handler,
+      GroupKeyStore keyStore,
+      KeyExchangeUtil keyExchangeUtil,
+      GroupKeyRequestFunction groupKeyRequestFunction,
+      long propagationTimeout,
+      long resendTimeout,
+      boolean skipGapsOnFullQueue) {
+    super(
+        streamId,
+        partition,
+        handler,
+        keyStore,
+        keyExchangeUtil,
+        groupKeyRequestFunction,
+        propagationTimeout,
+        resendTimeout,
+        skipGapsOnFullQueue);
+  }
 
-    public RealTimeSubscription(String streamId, int partition, MessageHandler handler, GroupKeyStore keyStore, KeyExchangeUtil keyExchangeUtil,
-                                GroupKeyRequestFunction groupKeyRequestFunction) {
-        this(streamId, partition, handler, keyStore, keyExchangeUtil, groupKeyRequestFunction, Subscription.DEFAULT_PROPAGATION_TIMEOUT,
-                Subscription.DEFAULT_RESEND_TIMEOUT, Subscription.DEFAULT_SKIP_GAPS_ON_FULL_QUEUE);
-    }
+  public RealTimeSubscription(
+      String streamId,
+      int partition,
+      MessageHandler handler,
+      GroupKeyStore keyStore,
+      KeyExchangeUtil keyExchangeUtil,
+      GroupKeyRequestFunction groupKeyRequestFunction) {
+    this(
+        streamId,
+        partition,
+        handler,
+        keyStore,
+        keyExchangeUtil,
+        groupKeyRequestFunction,
+        Subscription.DEFAULT_PROPAGATION_TIMEOUT,
+        Subscription.DEFAULT_RESEND_TIMEOUT,
+        Subscription.DEFAULT_SKIP_GAPS_ON_FULL_QUEUE);
+  }
 
-    @Override
-    public boolean isResending() {
-        return resending;
-    }
+  @Override
+  public boolean isResending() {
+    return resending;
+  }
 
-    @Override
-    public void setResending(boolean resending) {
-        this.resending = resending;
-    }
+  @Override
+  public void setResending(boolean resending) {
+    this.resending = resending;
+  }
 
-    @Override
-    public boolean hasResendOptions() {
-        return false;
-    }
+  @Override
+  public boolean hasResendOptions() {
+    return false;
+  }
 
-    @Override
-    public ResendOption getResendOption() {
-        return null;
-    }
+  @Override
+  public ResendOption getResendOption() {
+    return null;
+  }
 
-    @Override
-    public void startResend() {
-        resending = true;
-    }
+  @Override
+  public void startResend() {
+    resending = true;
+  }
 
-    @Override
-    public void endResend() throws GapDetectedException {
-        resending = false;
-        this.handler.done(this);
-    }
+  @Override
+  public void endResend() throws GapDetectedException {
+    resending = false;
+    this.handler.done(this);
+  }
 
-    @Override
-    public void handleRealTimeMessage(StreamMessage msg) throws GapDetectedException, UnsupportedMessageException {
-        orderingUtil.add(msg);
-    }
+  @Override
+  public void handleRealTimeMessage(StreamMessage msg)
+      throws GapDetectedException, UnsupportedMessageException {
+    orderingUtil.add(msg);
+  }
 
-    public void setLastMessageRefs(ArrayList<OrderedMsgChain> chains) {
-        orderingUtil.addChains(chains);
-    }
+  public void setLastMessageRefs(ArrayList<OrderedMsgChain> chains) {
+    orderingUtil.addChains(chains);
+  }
 
-    @Override
-    public Logger getLogger() {
-        return log;
-    }
+  @Override
+  public Logger getLogger() {
+    return log;
+  }
 }
