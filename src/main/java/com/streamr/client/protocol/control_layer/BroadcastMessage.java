9db84905bdaf08bf9bc03ca71423cd24ee34743d
Kare Nuorteva <kare.nuorteva@me.com>
2021-01-22 14:16:16 +0200

    Add BroadcastMessageTest for equals contract

--- src/main/java/com/streamr/client/protocol/control_layer/BroadcastMessage.java
+++ src/main/java/com/streamr/client/protocol/control_layer/BroadcastMessage.java
@@ -1,8 +1,9 @@
 package com.streamr.client.protocol.control_layer;
 
 import com.streamr.client.protocol.message_layer.StreamMessage;
+import java.util.Objects;
 
-public class BroadcastMessage extends ControlMessage {
+public final class BroadcastMessage extends ControlMessage {
     public static final int TYPE = 0;
 
     private final StreamMessage streamMessage;
@@ -14,6 +15,20 @@ public class BroadcastMessage extends ControlMessage {
 
     public StreamMessage getStreamMessage() {
         return streamMessage;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        final BroadcastMessage that = (BroadcastMessage) obj;
+        return getType() == that.getType() && Objects.equals(getRequestId(), that.getRequestId()) &&
+            Objects.equals(streamMessage, that.streamMessage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(streamMessage, getType(), getRequestId());
     }
 
     @Override
