f4d7bb298d612a1a4956ec85da518555180b1cd7
Jonathan Wolff <jwolff@isentropy.com>
2021-03-12 12:42:12 +0100

    add maxgas arg to test

--- src/integrationTest/groovy/com/streamr/client/DataUnionClientSpec.groovy
+++ src/integrationTest/groovy/com/streamr/client/DataUnionClientSpec.groovy
@@ -42,7 +42,7 @@ class DataUnionClientSpec extends StreamrIntegrationSpecification{
         du = client.dataUnionFromName(duname);
         //    public static IERC20 load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider)
         Web3j mainnet = Web3j.build(new HttpService(DEV_MAINCHAIN_RPC))
-        mainnetToken = IERC20.load(client.mainnetTokenAddress(), mainnet, wallets[0], new EstimatedGasProvider(mainnet))
+        mainnetToken = IERC20.load(client.mainnetTokenAddress(), mainnet, wallets[0], new EstimatedGasProvider(mainnet, 730000))
     }
 
     void "create DU"() {
