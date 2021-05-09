a7f494f61c77baca48202a5c7decf25da8351535
Kare Nuorteva <kare.nuorteva@me.com>
2021-05-09 22:03:12 +0300

    Remove SignatureComputationPolicy

--- client/src/main/java/com/streamr/client/options/SigningOptions.java
+++ client/src/main/java/com/streamr/client/options/SigningOptions.java
@@ -1,24 +1,18 @@
 package com.streamr.client.options;
 
 public class SigningOptions {
-    private final SignatureVerificationPolicy verifySignatures;
+  private final SignatureVerificationPolicy verifySignatures;
 
-    public enum SignatureComputationPolicy {
-        AUTO,
-        ALWAYS,
-        NEVER
-    }
+  public enum SignatureVerificationPolicy {
+    AUTO,
+    ALWAYS
+  }
 
-    public enum SignatureVerificationPolicy {
-        AUTO,
-        ALWAYS
-    }
+  public SigningOptions(SignatureVerificationPolicy verifySignatures) {
+    this.verifySignatures = verifySignatures;
+  }
 
-    public SigningOptions(SignatureVerificationPolicy verifySignatures) {
-        this.verifySignatures = verifySignatures;
-    }
-
-    public SignatureVerificationPolicy getVerifySignatures() {
-        return verifySignatures;
-    }
+  public SignatureVerificationPolicy getVerifySignatures() {
+    return verifySignatures;
+  }
 }
