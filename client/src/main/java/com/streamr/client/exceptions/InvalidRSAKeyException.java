d01a3fe970dec0e2847fdb35dbc085ed1f6993ad
Kare Nuorteva <kare.nuorteva@me.com>
2021-05-09 18:26:49 +0300

    Remove InvalidRSAKeyException

--- client/src/main/java/com/streamr/client/exceptions/InvalidRSAKeyException.java
+++
@@ -1,9 +0,0 @@
-package com.streamr.client.exceptions;
-
-public class InvalidRSAKeyException extends RuntimeException {
-
-    public InvalidRSAKeyException(boolean isPublic) {
-        super("Must be a valid RSA " + (isPublic ? "public" : "private") + " key in the PEM format.");
-    }
-
-}
