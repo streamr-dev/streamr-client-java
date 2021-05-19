037846262958f92b1ef4084388880a19cbb628a6
Jonathan Wolff <jwolff@isentropy.com>
2021-05-20 00:41:28 +0200

    amb proxy address

--- src/main/java/com/streamr/client/options/DataUnionClientOptions.java
+++ src/main/java/com/streamr/client/options/DataUnionClientOptions.java
@@ -6,7 +6,7 @@ public class DataUnionClientOptions {
     private String dataUnionSidechainFactoryAddress = "0x1b55587Beea0b5Bc96Bb2ADa56bD692870522e9f";
     private String dataUnionMainnetFactoryAddress = "0x7d55f9981d4E10A193314E001b96f72FCc901e40";
     private String binanceAdapterAddress = "0x0c1aF6edA561fbDA48E9A7B1Dd46D216F31A97cC";
-    private String binanceSmartChainAMBAddress = "0xa93eE7B4a7215F7E725437a6b6d7A4E7Fe1Dd8f0";
+    private String binanceSmartChainAMBAddress = "0x05185872898b6f94aa600177ef41b9334b1fa48b";
     private String mainnetAdminPrvKey;
     private String sidechainAdminPrvKey;
     private String binanceRPC = "https://bsc-dataseed.binance.org/";
