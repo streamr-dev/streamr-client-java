e6d2024dcc7b5e7ed1bba016093e30ca0f3e1d58
Juuso Takalainen <juuso@streamr.network>
2021-12-01 14:23:03 +0200

    fix: update addresses
    The REAL fix of course is ETH-193, using the config package

--- src/main/java/com/streamr/client/options/DataUnionClientOptions.java
+++ src/main/java/com/streamr/client/options/DataUnionClientOptions.java
@@ -3,9 +3,9 @@ package com.streamr.client.options;
 public class DataUnionClientOptions {
 
     //mainnet - xDai addresses are default:
-    private String dataUnionSidechainFactoryAddress = "0x1b55587Beea0b5Bc96Bb2ADa56bD692870522e9f";
-    private String dataUnionMainnetFactoryAddress = "0x7d55f9981d4E10A193314E001b96f72FCc901e40";
-    private String binanceAdapterAddress = "0x0c1aF6edA561fbDA48E9A7B1Dd46D216F31A97cC";
+    private String dataUnionSidechainFactoryAddress = "0xFCE1FBFAaE61861B011B379442c8eE1DC868ABd0";
+    private String dataUnionMainnetFactoryAddress = "0xE41439BF434F9CfBF0153f5231C205d4ae0C22e3";
+    private String binanceAdapterAddress = "0x193888692673b5dD46e6BC90bA8cBFeDa515c8C1";
     private String binanceSmartChainAMBAddress = "0x05185872898b6f94aa600177ef41b9334b1fa48b";
     private String mainnetAdminPrivateKey;
     private String sidechainAdminPrivateKey;
